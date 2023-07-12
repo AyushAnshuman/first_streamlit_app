d208840128ade1a6f79765a1f42307fe2052f35b
AyushAnshuman <85148683+AyushAnshuman@users.noreply.github.com>
2023-07-12 22:27:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oamlet')
+streamlit.text('Kale, Spinach, & Rocket smoothie')
+streamlit.text('Hard bolied free range egg')
