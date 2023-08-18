4e305f6c5e90e951266e4e8b358765a51ec489bc
AyushAnshuman <85148683+AyushAnshuman@users.noreply.github.com>
2023-08-18 23:33:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #New Section to display fruitvice api response
 import requests
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
