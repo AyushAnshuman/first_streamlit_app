f89eb395d51254d5cb0501b545effbe3f15c746d
AyushAnshuman <85148683+AyushAnshuman@users.noreply.github.com>
2023-08-18 23:21:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My parents new healthy dinner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
